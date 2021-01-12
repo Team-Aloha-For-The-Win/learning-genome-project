b1d739d19b1e8f9c48c0325eb5137c67a4f09d77
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-12 09:32:51 -1000

    first commit

---
+++ javasccript_sandbox.js
@@ -0,0 +1,11 @@
+const thisObject = [
+    {
+        name: "tony",
+        age: 2,
+        job: "being a child"
+    }
+]
+
+const pulledArray = thisObject.map( element => {
+    return element;
+})
