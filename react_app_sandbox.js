164ff279ef0a5fe6625ecb048a99615fdb90e3a6
artofmayhem <chef_tony_miller@yahoo.com>
2021-04-14 09:52:47 -1000

    added useDarkMode

--- react_app_sandbox.js
+++ react_app_sandbox.js
@@ -205,3 +205,36 @@ const validateChange = (e) => {
       });
   }
 };
+
+//useDarkMode custom hook
+
+import useLocalStorage  from './useLocalStorage'
+
+const useDarkMode = (key, value) => {
+    const [ darkMode, setDarkMode ] = useLocalStorage(key, value)
+    return [ darkMode, setDarkMode ]
+}
+
+export default useDarkMode
+
+
+// this snippet stores input values into local storage
+import { useState } from "react";
+export const useInput = (key, initialValue) => {
+  const [value, setValue] = useLocalStorage(key, initialValue);
+  const handleChanges = updatedValue => {
+    setValue(updatedValue);
+  };
+  return [value, setValue, handleChanges];
+};
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+};
